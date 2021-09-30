028389eeb4770fac7a38f3f5e3e0bef5e0121690
Lucas Schmidt <lucas.schmidt015@gmail.com>
2021-09-30 18:37:33 -0300

    Power

--- pilha.c
+++ pilha.c
@@ -18,14 +18,31 @@ void printer(end *last){
 int main(){
     end *last=NULL, *pilha, *aux;
     int acelerador=1;
-    char teste[60]={"B"};
     while(acelerador){
         pilha = (end *)malloc(sizeof(end));
-        printf("Digita ai: \n");
         scanf("%s", pilha->link);
         pilha->prev=NULL;
-        if(strcpy(pilha->link,teste)==0){
+        if(strcmp(pilha->link,"E")==0){
+            if(last==NULL){
+                printf("!\n");
+            }else{
+                while(last!=NULL){
+                    aux = last;
+                    last=last->prev;
+                    free(aux);
+                    printf("@\n");
+                }
+            }
             break;
+        }else if(strcmp(pilha->link,"B")==0){
+            if(last==NULL){
+                printf("Vazio\n");
+            }else{
+                printf("%s\n", last->link);
+                aux = last;
+                last = last->prev;
+                free(aux);
+            }
         }
         else{
             if(last==NULL){
